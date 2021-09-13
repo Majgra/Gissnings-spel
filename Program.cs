48b3868e6dd31483026a28a01351427c235da688
Majgra <90604565+Majgra@users.noreply.github.com>
2021-09-13 10:23:57 +0200

    Gissnings spel

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Projekt_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int slump_tal = rand.Next(1,101);
+            Console.WriteLine ("Gissa på ett tal mellan 1 och 100");
+            int tal = Convert.ToInt32(Console.ReadLine());
+            while (tal != slump_tal)
+            {
+                if (tal < slump_tal)
+                {
+                    Console.WriteLine ("Talet är för lågt. Gissa igen!");
+                }
+                else if (tal > slump_tal)
+                {
+                    Console.WriteLine ("Talet är för högt. Gissa igen!");
+                }
+                tal = Convert.ToInt32 (Console.ReadLine());
+    
+            }
+
+            Console.WriteLine ("Du gissade rätt!");
+            Console.WriteLine("Det tog så här många försök: ");
+            
+        }
+    }
+}
